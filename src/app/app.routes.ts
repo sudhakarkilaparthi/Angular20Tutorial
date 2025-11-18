a8f1ff9352679862aa84d419ca7340472f4431f4
Sudhakara Rao Kilaparthi <sudhakara@blacklake.in>
2025-11-18 18:39:14 +0530

    Routing concept added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { ControlFlow } from './control-flow/control-flow';
+import { User } from './components/user/user';
+import { DataBinding } from './components/data-binding/data-binding';
+import { SignalEx } from './components/signal-ex/signal-ex';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+            path:'admin',component:Admin
+      },
+      {
+            path:'control-flow-statement', component:ControlFlow
+      },
+      {
+            path:'user',component:User
+      },
+      {
+            path:'data-binding',component:DataBinding
+      },
+      {
+            path:'signal-ex',component:SignalEx
+      }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { Admin } from './components/admin/admin';
 import { User } from './components/user/user';
 import { DataBinding } from './components/data-binding/data-binding';
 
 @Component({
   selector: 'app-root',
-  imports: [Admin, User, DataBinding],
+  imports: [Admin, User, DataBinding, RouterLink, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
