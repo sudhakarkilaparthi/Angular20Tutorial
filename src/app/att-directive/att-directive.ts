43ae3d03fd20bb22ab6298de4965f15174da731e
Sudhakara Rao Kilaparthi <sudhakara@blacklake.in>
2025-11-18 21:19:59 +0530

    Component life cycle, directives and pipes

--- src/app/att-directive/att-directive.ts
+++ src/app/att-directive/att-directive.ts
@@ -1,16 +1,66 @@
-import { Component, signal } from '@angular/core';
-import { NgClass, NgStyle } from '@angular/common';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { DatePipe, JsonPipe, LowerCasePipe, NgClass, NgStyle, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-att-directive',
-  imports: [NgClass, NgStyle],
+  imports: [NgClass, NgStyle, UpperCasePipe, LowerCasePipe, TitleCasePipe, SlicePipe, JsonPipe, DatePipe],
   templateUrl: './att-directive.html',
   styleUrl: './att-directive.css',
 })
-export class AttDirective {
+export class AttDirective implements OnInit, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
   div1ClassName = signal<string>("");
 
+  firstName: string = "Sudhakar";
+  courseName: string = "Angular 20 tutorial";
+
+  rollNoList = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+  studentDetails: any = {
+    name: "Sudhakar",
+    age: 33,
+    city: "Hyderabad",
+    state: "Telangana"
+  }
+
+  currentDate: Date = new Date();
+
   isDiv2Green: boolean = false;
+
+
+  constructor() {
+    console.log("constructor");
+  }
+
+  ngOnInit(): void {
+    console.log("ngOnInit");
+    // to trigger API calls
+    //to subscribe 
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+    //to deal with viewChild
+    //subscribe
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy");
+    //unsubscribe
+    //clean up
+    //restrict use from navigating away
+  }
+
+
 
   setBgClass(className: string) {
     this.div1ClassName.set(className);
