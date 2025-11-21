c007738656c7f9307da4453b88790d964cf8f521
Sudhakara Rao Kilaparthi <sudhakara@blacklake.in>
2025-11-21 22:50:34 +0530

    Services example

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -2,6 +2,7 @@ import { JsonPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Master } from '../../services/master';
 
 @Component({
   selector: 'app-user',
@@ -10,51 +11,62 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user.css',
 })
 export class User implements OnInit {
-
   http = inject(HttpClient);
+  masterService = inject(Master);
+
+  resultSum = this.masterService.getSum(2, 5);
+
   usersList: any = [];
 
   userObj: any = {
-    "userId": 0,
-    "emailId": "",
-    "password": "",
-    "fullName": "",
-    "mobileNo": ""
-  }
+    userId: 0,
+    emailId: '',
+    password: '',
+    fullName: '',
+    mobileNo: '',
+  };
 
   ngOnInit(): void {
     this.getUsers();
   }
 
   getUsers() {
-    this.http.get('https://api.freeprojectapi.com/api/GoalTracker/getAllUsers').subscribe((result: any) => {
+    // this.http
+    //   .get('https://api.freeprojectapi.com/api/GoalTracker/getAllUsers')
+    //   .subscribe((result: any) => {
+    //     this.usersList = result;
+    //   });
+
+    this.masterService.getUsers().subscribe((result: any) => {
       this.usersList = result;
     });
   }
 
   OnSaveUser() {
     debugger;
-    this.http.post('https://api.freeprojectapi.com/api/GoalTracker/register', this.userObj).subscribe({
-      next: (result) => {
-        debugger;
-        alert("User Registered Successfully");
-        this.getUsers();
-      },
-      error: (error) => {
-        debugger;
-        alert("Error:" + JSON.stringify(error));
-      }
-    });
+    this.http
+      .post('https://api.freeprojectapi.com/api/GoalTracker/register', this.userObj)
+      .subscribe({
+        next: (result) => {
+          debugger;
+          alert('User Registered Successfully');
+          this.getUsers();
+        },
+        error: (error) => {
+          debugger;
+          alert('Error:' + JSON.stringify(error));
+        },
+      });
   }
 
   OnReset() {
     this.userObj = {
-      "userId": 0,
-      "emailId": "",
-      "password": "",
-      "fullName": "",
-      "mobileNo": ""
-    }
+      userId: 0,
+      emailId: '',
+      password: '',
+      fullName: '',
+      mobileNo: '',
+    };
   }
   OnEditUser(user: any) {
     this.userObj = user;
@@ -62,26 +74,34 @@ export class User implements OnInit {
 
   OnUpdateUser() {
     this.userObj.createdDate = new Date();
-    this.http.put('https://api.freeprojectapi.com/api/GoalTracker/updateUser?id=' + this.userObj.userId, this.userObj).subscribe({
-      next: (result) => {
-        alert("User Updated Successfully");
-        this.getUsers();
-      }, error: (error) => {
-        alert("Error:" + error.error.message);
-      }
-    })
+    this.http
+      .put(
+        'https://api.freeprojectapi.com/api/GoalTracker/updateUser?id=' + this.userObj.userId,
+        this.userObj
+      )
+      .subscribe({
+        next: (result) => {
+          alert('User Updated Successfully');
+          this.getUsers();
+        },
+        error: (error) => {
+          alert('Error:' + error.error.message);
+        },
+      });
   }
 
   OnDeleteUser(userId: number) {
-    const confirmDelete = confirm("Are you sure you want to delete this user?");
-    this.http.delete('https://api.freeprojectapi.com/api/GoalTracker/deleteUser?id=' + userId).subscribe({
-      next: (result) => {
-        alert("User Deleted Successfully");
-        this.getUsers();
-      }, error: (error) => {
-        alert("Error:" + error.error.message);
-      }
-    })
+    const confirmDelete = confirm('Are you sure you want to delete this user?');
+    this.http
+      .delete('https://api.freeprojectapi.com/api/GoalTracker/deleteUser?id=' + userId)
+      .subscribe({
+        next: (result) => {
+          alert('User Deleted Successfully');
+          this.getUsers();
+        },
+        error: (error) => {
+          alert('Error:' + error.error.message);
+        },
+      });
   }
-
 }
